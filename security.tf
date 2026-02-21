3a64921055ce133df746a04beeca24f13fe61aab
yoezc <yoezcode@outlook.de>
2026-02-22 00:16:27 +0100

    added security group for db

--- security.tf
+++ security.tf
@@ -31,6 +31,25 @@ resource "aws_security_group" "web" {
   }
 }
 
+resource "aws_security_group" "db" {
+  name   = "wp-db-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_key_pair" "capstone" {
   key_name   = "capstone-key"
   public_key = file(var.public_key_path)
