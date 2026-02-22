dd8b2ee1e0d2819f7a5a0c9321ba2ebe8907c4fd
yoezc <yoezcode@outlook.de>
2026-02-22 22:31:05 +0100

    removed old single instance

--- ec2.tf
+++ ec2.tf
@@ -8,24 +8,3 @@ data "aws_ami" "amazon_linux" {
     values = ["al2023-ami-*-x86_64"]
   }
 }
-
-resource "aws_instance" "wordpress" {
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private_a.id
-  vpc_security_group_ids      = [aws_security_group.web.id]
-  associate_public_ip_address = false
-  key_name                    = aws_key_pair.capstone.key_name
-
-  user_data = templatefile("${path.module}/userdata.sh.tmpl", {
-    db_name     = var.db_name
-    db_user     = var.db_user
-    db_password = var.db_password
-    db_host     = aws_db_instance.wp.endpoint
-  })
-  depends_on = [aws_nat_gateway.nat, aws_db_instance.wp]
-
-  tags = {
-    Name = "wordpress-webserver"
-  }
-}
