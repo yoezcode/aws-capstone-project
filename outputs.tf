c275dc767a28687bc060b96b2d31f1207bb9748c
yoezc <yoezcode@outlook.de>
2026-02-22 22:34:00 +0100

    added new output inof

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
-output "wordpress_url" {
-  value = "http://${aws_instance.wordpress.public_ip}"
-}
-
 output "rds_endpoint" {
   value = aws_db_instance.wp.address
 }
 
-output "bastion" {
+output "bastion_ip" {
   value = aws_instance.bastion.public_ip
+}
+
+output "alb_url" {
+  value = aws_lb.wp.dns_name
+}
+
+output "asg_name" {
+  value = aws_autoscaling_group.wp.name
 }
